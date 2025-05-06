c6d506ad067661d33d96945f41eb2a1a62fa1589
Anastasia <carrie@mail.ru>
2025-05-06 13:56:45 +0300

    Исправлена шапка и кнопка вход/выход

--- project/src/components/header/header.tsx
+++ project/src/components/header/header.tsx
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { AppRoute } from '../../const';
+import {AppRoute, AuthorizationStatus} from '../../const';
 import {useAppSelector} from '../../hooks';
 import {getAuthorizationStatus, getUser} from '../../store/user-process/selectors';
 import Logo from '../logo/logo';
@@ -17,13 +17,15 @@ const Header = ({page}: HeaderProps) => {
       <Logo page={page} />
 
       <ul className="user-block">
+        {authorizationStatus === AuthorizationStatus.Auth && (
+          <li className="user-block__item">
+            <div className="user-block__avatar">
+              <img src={user} alt="User avatar" width="63" height="63"/>
+            </div>
+          </li>
+        )}
         <li className="user-block__item">
-          <div className="user-block__avatar">
-            <img src={user} alt="User avatar" width="63" height="63"/>
-          </div>
-        </li>
-        <li className="user-block__item">
-          <Link className="user-block__link" to={AppRoute.Login}>Sign out</Link>
+          <Link className="user-block__link" to={AppRoute.Login}>{authorizationStatus === AuthorizationStatus.Auth ? 'Sign out' : 'Sign in'}</Link>
         </li>
       </ul>
     </header>
